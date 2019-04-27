844393cddb41159b41b7c131a1500c04d7dcb40e
un4ckn0wl3z <slickerth@gmail.com>
2019-04-28 00:00:54 +0700

    add linker

---
+++ InDepthCppVS/Linker/SystemHelloWorl/Log.h
@@ -0,0 +1,25 @@
+#pragma once
+
+
+void Log(const char* message);
+
+
+
+
+
+
+
+/*
+static void Log(const char* message)
+{
+	std::cout << message << std::endl;
+}
+
+*/
+
+/*
+inline void Log(const char* message)
+{
+	std::cout << message << std::endl;
+}
+*/
---
+++ InDepthCppVS/Linker/SystemHelloWorl/Math.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include "Log.h"
+
+
+static int Multiply(int a, int b)
+{
+	Log("Multiply");
+	return a * b;
+}
+
+int main() 
+{
+
+	std::cout << Multiply(5,8) << std::endl;
+	std::cin.get();
+
+}
