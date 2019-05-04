04d33195beb0c32719525f6b05c2420e1375879c
un4ckn0wl3z <slickerth@gmail.com>
2019-05-04 19:36:22 +0700

    add log

---
+++ InDepthCppVS/Classs-Log/SystemHelloWorl/Main.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+
+class Log
+{
+public:
+	const int LogLevelError = 0;
+	const int LogLevelWarning = 1;
+	const int LogLevelInfo = 2;
+private:
+	int m_Loglevel = LogLevelInfo;
+
+public:
+	void SetLevel(int level)
+	{
+		m_Loglevel = level;
+	}
+
+	void Error(const char* message)
+	{
+		if (m_Loglevel >= LogLevelError)
+			std::cout << "[ERROR]: " << message << std::endl;
+	}
+
+	void Warn(const char* message)
+	{
+		if (m_Loglevel >= LogLevelWarning)
+			std::cout << "[WARNING]: " << message << std::endl;
+	}
+
+	void Info(const char* message)
+	{
+		if (m_Loglevel >= LogLevelInfo)
+			std::cout << "[INFO]: " << message << std::endl;
+	}
+
+
+};
+
+
+int main()
+{
+	
+	Log log;
+
+	//log.SetLevel(log.LogLevelWarning);
+	log.Error("Hello!");
+	log.Warn("Hello!");
+	log.Info("Hello!");
+
+	std::cin.get();
+}
