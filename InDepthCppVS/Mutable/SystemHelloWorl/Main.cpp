aca81d15eb524d77b9039efbdf173b535e4441e3
un4ckn0wl3z <slickerth@gmail.com>
2019-05-07 21:20:27 +0700

    add mutable

---
+++ InDepthCppVS/Mutable/SystemHelloWorl/Main.cpp
@@ -0,0 +1,41 @@
+﻿#include <iostream>
+#include <string>
+
+
+#define LOG(x) std::cout << x << std::endl;
+
+
+
+class Entity
+{
+private:
+	std::string m_Name;
+	mutable int m_DebugCount = 0;
+public:
+	const std::string& GetName() const
+	{
+		m_DebugCount++;
+		return m_Name;
+	}
+};
+
+
+int main()
+{
+
+	const Entity e;
+	e.GetName();
+
+	int x = 8;
+
+	auto f = [=]() mutable
+	{	
+		x++;
+		LOG(x);
+	};
+
+	f();
+	LOG(x);
+
+	std::cin.get();
+}
