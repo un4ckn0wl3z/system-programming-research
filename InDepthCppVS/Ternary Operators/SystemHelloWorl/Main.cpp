680dd6d253aa7cc9407f2a6dc54b4f37a8450422
un4ckn0wl3z <slickerth@gmail.com>
2019-05-07 22:06:21 +0700

    add ternary

---
+++ InDepthCppVS/Ternary Operators/SystemHelloWorl/Main.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+#include <string>
+
+
+#define LOG(x) std::cout << x << std::endl;
+
+static int s_Level = 1;
+static int s_Speed = 2;
+
+
+int main()
+{
+	s_Speed = s_Level > 5 ? 10 : 5;
+
+	LOG(s_Speed);
+
+	std::cin.get();
+}
