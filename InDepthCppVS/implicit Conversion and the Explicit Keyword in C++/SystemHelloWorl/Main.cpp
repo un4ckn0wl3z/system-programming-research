b6ef8027e314ca918b3b282a732c2dbb4986fe37
un4ckn0wl3z <slickerth@gmail.com>
2019-05-08 22:00:26 +0700

    add cast

---
+++ InDepthCppVS/implicit Conversion and the Explicit Keyword in C++/SystemHelloWorl/Main.cpp
@@ -0,0 +1,33 @@
+﻿#include <iostream>
+#include <string>
+
+
+#define LOG(x) std::cout << x << std::endl;
+
+
+class Entity
+{
+private:
+	std::string m_Name;
+	int m_Age;
+public:
+	explicit Entity(const std::string& name)
+		: m_Name(name), m_Age(-1)
+	{}
+
+	explicit Entity(int age)
+		: m_Name("Unk"), m_Age(age)
+	{}
+};
+
+
+
+int main()
+{
+
+	Entity a(10);
+	Entity b((std::string)"Anuwat");
+	
+	
+	std::cin.get();
+}
