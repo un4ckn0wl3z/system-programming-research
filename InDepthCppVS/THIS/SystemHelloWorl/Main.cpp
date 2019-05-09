538d2d0aebd8f6d0e19d949e73f17c84ecc57791
un4ckn0wl3z <slickerth@gmail.com>
2019-05-09 21:46:52 +0700

    adad this

---
+++ InDepthCppVS/THIS/SystemHelloWorl/Main.cpp
@@ -0,0 +1,49 @@
+﻿#include <iostream>
+#include <string>
+
+
+#define LOG(x) std::cout << x << std::endl;
+
+
+
+void Logr(const char* message);
+void PrintEntity(const Entity& e);
+
+
+class Entity
+{
+public:
+	int x, y;
+
+
+	Entity() {}
+
+	Entity(int x, int y) //: x(x), y(y)
+	{
+		this->x = x;
+		this->y = y;
+
+		Entity& e = *this;
+		PrintEntity(*this);
+
+	}
+
+};
+
+void Logr(const char* message)
+{
+	std::cout << message << std::endl;
+}
+
+void PrintEntity(const Entity& e)
+{
+
+}
+
+int main()
+{
+	
+	
+	
+	std::cin.get();
+}
