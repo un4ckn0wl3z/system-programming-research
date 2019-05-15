65b4e103dbc35744d0214cf7fa50088f37903b06
un4ckn0wl3z <slickerth@gmail.com>
2019-05-15 21:53:50 +0700

    add smart pointer

---
+++ InDepthCppVS/SMART POINTERS in C++/SystemHelloWorl/Main.cpp
@@ -0,0 +1,48 @@
+﻿#include <iostream>
+#include <string>
+#include <memory>
+
+
+
+#define LOG(x) std::cout << x << std::endl;
+
+class Entity
+{
+public:
+
+	Entity() 
+	{
+		LOG("Created Object!");
+	}
+
+	~Entity()
+	{
+		LOG("Destroyed Object!");
+	}
+
+	void Print() {}
+};
+
+
+
+int main()
+{
+	{
+		std::weak_ptr<Entity> ew;
+
+		std::shared_ptr<Entity> e0;
+		{
+			std::unique_ptr<Entity> entity = std::make_unique<Entity>();
+			entity->Print();
+
+			std::shared_ptr<Entity> sharedEntity = std::make_shared<Entity>();
+
+			e0 = sharedEntity;
+
+			ew = sharedEntity;
+
+		}
+	}
+
+	std::cin.get();
+}
