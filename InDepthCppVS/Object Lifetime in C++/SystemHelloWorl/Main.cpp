86399b800c47d02aa3e397a9dc14e5962b616afc
un4ckn0wl3z <slickerth@gmail.com>
2019-05-15 21:16:35 +0700

    add scope in c++

---
+++ InDepthCppVS/Object Lifetime in C++/SystemHelloWorl/Main.cpp
@@ -0,0 +1,71 @@
+﻿#include <iostream>
+#include <string>
+
+
+#define LOG(x) std::cout << x << std::endl;
+
+class Entity
+{
+public:
+
+	Entity() 
+	{
+		LOG("Created Object!");
+	}
+
+	~Entity()
+	{
+		LOG("Destroyed Object!");
+	}
+};
+
+int* CreateArray()
+{
+	int* array = new int[50];
+	return array;
+}
+
+void CreateArray2(int* array)
+{
+	// file array
+
+}
+
+
+class ScopePtr
+{
+private:
+	Entity * m_Ptr;
+public:
+	ScopePtr(Entity * ptr) : m_Ptr(ptr) {}
+	~ScopePtr() { delete m_Ptr; }
+};
+
+
+int main()
+{
+
+	{
+		Entity e;
+	
+	}
+
+	{
+		Entity* e = new Entity();
+
+	}
+
+	{
+	
+		ScopePtr e = new Entity();
+
+	}
+
+	int * a = CreateArray();
+	
+	int a2[50];
+	CreateArray2(a2);
+	
+	
+	std::cin.get();
+}
