c8e0c32a4ae05c3fb27f739a027f0f79bbe91786
un4ckn0wl3z <slickerth@gmail.com>
2019-05-19 23:01:52 +0700

    add static array

---
+++ InDepthCppVS/Static Arrays in C++ (stdarray)/ProdCpp/Game/Game/src/Application.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <array>
+
+
+void PrintArray(const std::array<int,5>& data)
+{
+	for (int i=0;i<data.size();i++)
+	{
+
+	}
+}
+
+int main()
+{
+	std::array<int, 5> data;
+	data[0] = 2;
+	data[1] = 1;
+	
+	std::cin.get();
+}
